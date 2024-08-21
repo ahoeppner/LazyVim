c4ddf0409e6c295755372eb24e76ed24c4427b92
Alex Hoeppner <alex@hwlocal.net>
2024-08-21 11:46:25 -0500

    added more colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,38 @@
 return {
-  -- add gruvbox
+  -- add colorschemes I like
   { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    },
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night", -- moon, storm, night, day
+    },
+  },
+
+  {
+    "ribru17/bamboo.nvim",
+    opts = {
+      style = "vulgaris", -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
+    },
+  },
+
+  {
+    "neanias/everforest-nvim",
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "everforest",
     },
   },
 }
